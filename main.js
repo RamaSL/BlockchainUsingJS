5a51c8e00b4574a9bd7e4902c7348201f172e720
Dhanuka Ramanayake <dhanuka.rama@gmail.com>
2019-02-15 12:40:58 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+
+const SHA256 = require('crypto-js/sha256');
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "13/02/2019", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
+let ramaCoin = new Blockchain();
+ramaCoin.addBlock(new Block(1, "14/02/2019", "blockchain is awesome"));
+ramaCoin.addBlock(new Block(2, "15/02/2019", { amount: 001 }));
+ramaCoin.addBlock(new Block(3, "16/02/2019", { amount: 002 }));
+
+console.log(JSON.stringify(ramaCoin, null, 4));
+console.log('is blockchain valid? ' + ramaCoin.isChainValid());
