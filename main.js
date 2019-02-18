30f33d3eab35fc0e143d3660ff09ab6aea72bb1b
Dhanuka Ramanayake <dhanuka.rama@gmail.com>
2019-02-18 21:37:02 +0530

    formatting

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class Block {
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+this.nonce).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
   }
 
   mineBlock(difficulty) {
